1feff01d9cffe26883796b77d2a501090948a0fe
jackyscript <8930712+jackyscript@users.noreply.github.com>
2025-05-16 02:32:30 +0200

    include app base url for favicon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,7 +6,16 @@ export default defineNuxtConfig({
   modules: ["nuxt-svgo"],
   app: {
     head: {
-      link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
+      link: [
+        {
+          rel: "icon",
+          type: "image/x-icon",
+          href:
+            (import.meta.env.NUXT_APP_BASE_URL
+              ? import.meta.env.NUXT_APP_BASE_URL
+              : "/") + "favicon.ico",
+        },
+      ],
     },
     baseURL: "/jackyscript/",
   },
