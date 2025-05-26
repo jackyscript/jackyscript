6ddef486f06696decd3bbfbdd3146c60ca3a40c8
jackyscript <8930712+jackyscript@users.noreply.github.com>
2025-05-26 09:11:13 +0200

    check theming

--- components/MenuComponent.vue
+++ components/MenuComponent.vue
@@ -1,10 +1,25 @@
 <script setup>
+import { computed } from "vue";
 function darken() {
-  document.documentElement.setAttribute('data-theme', 'dark')
+  currentTheme.value = "dark";
+  document.documentElement.setAttribute("data-theme", "dark");
 }
 function brighten() {
-  document.documentElement.setAttribute('data-theme', 'light')
+  currentTheme.value = "light";
+  document.documentElement.setAttribute("data-theme", "light");
 }
+
+const preferredTheme = computed(() => {
+  if (import.meta.client) {
+  // Check if the client supports prefers-color-scheme
+    return window.matchMedia("(prefers-color-scheme: dark)").matches
+      ? "dark"
+      : "light";
+  }
+  return "light"; // Default to light if not in client context
+});
+
+const currentTheme = ref(preferredTheme.value);
 </script>
 
 <template>
@@ -13,13 +28,13 @@ function brighten() {
       <li><strong>Menu</strong></li>
     </ul>
     <ul>
-      <li>
+      <li v-if="currentTheme === 'light'">
         <button role="button" title="Darken" @click="darken">
           <svgo-darken aria-hidden="true" class="logo" alt="Darken" />
           <span hidden>Darken</span>
         </button>
       </li>
-      <li>
+      <li v-else>
         <button role="button" title="Brighten" @click="brighten">
           <svgo-brighten aria-hidden="true" class="logo" alt="Brighten" />
           <span hidden>Brighten</span>
