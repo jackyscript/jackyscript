cf84e6bd849e0e427b5f9f7e8456ea646194ecbc
dejafu <8930712+jackyscript@users.noreply.github.com>
2025-06-05 21:17:39 +0200

    Fix typo

--- app.vue
+++ app.vue
@@ -20,7 +20,7 @@
               <ul dir="rtl">
                 <li><NuxtLink to="/experience">Experience</NuxtLink></li>
                 <li><NuxtLink to="/education">Education</NuxtLink></li>
-                <li><NuxtLink to="/publication">Publications</NuxtLink></li>
+                <li><NuxtLink to="/publication">Publication</NuxtLink></li>
                 <li><NuxtLink to="/volunteering">Volunteering</NuxtLink></li>
               </ul>
             </details>
