23af68472f0164d874d7f2134427e276d07882b6
jackyscript <8930712+jackyscript@users.noreply.github.com>
2025-06-08 22:39:08 +0200

    update volunteering

--- components/TheVolunteering.vue
+++ components/TheVolunteering.vue
@@ -10,6 +10,7 @@ import VolunteeringItem from './VolunteeringItem.vue'
     <template #role>Volunteer</template>
     <template #date>since Oct. 2024</template>
     <p>codebar is a charity that enables the growth of a diverse tech community by providing a mutual exchange platform and running free programming workshops for fellow people aspiring and engaging in tech.</p>
+    <p>I answer questions about programming and related topics, and offer advice on the community platform.</p>
   </VolunteeringItem>
 
   <VolunteeringItem>
@@ -19,6 +20,7 @@ import VolunteeringItem from './VolunteeringItem.vue'
     <template #role>Volunteer @ Dropping Hub</template>
     <template #date>since Feb. 2025</template>
     <p>Labdoo is a non-profit global platform where people around the world collaborate to bring secondhand laptops loaded with educational software to schools and people in need using CO2-neutral means.</p>
+    <p>I maintain a dropping hub, manage donated devices, and forward them accordingly.</p>
   </VolunteeringItem>
 </template>
 
