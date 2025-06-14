5466e383fa0acc23b5fe2cff4d6e04d1c26ae3a9
jackyscript <8930712+jackyscript@users.noreply.github.com>
2025-06-15 00:23:48 +0200

    add footer info

--- app.vue
+++ app.vue
@@ -6,5 +6,9 @@
 
     <footer class="container">
       <a href="#top">Go to top</a>
+      <hr >
+      <section>
+      <small>This website is made with care using <a href="https://nuxt.com" target="_blank" rel="noopener">Nuxt</a> and <a href="https://picocss.com" target="_blank" rel="noopener">Pico CSS</a> ✨.</small>
+      </section>
     </footer>
 </template>
