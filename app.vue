ddbd4334ba595aac08fb5a1cb725dc548406517b
jackyscript <8930712+jackyscript@users.noreply.github.com>
2025-06-16 02:51:58 +0200

    add link to code repository

--- app.vue
+++ app.vue
@@ -1,14 +1,27 @@
 <template>
-    <NuxtRouteAnnouncer />
-    <HeaderComponent> <MenuComponent /></HeaderComponent>
+  <NuxtRouteAnnouncer />
+  <HeaderComponent> <MenuComponent /></HeaderComponent>
 
-    <NuxtPage />
+  <NuxtPage />
 
-    <footer class="container">
-      <a href="#top">Go to top</a>
-      <hr >
-      <section>
-      <small>This website is made with care using <a href="https://nuxt.com" target="_blank" rel="noopener">Nuxt</a> and <a href="https://picocss.com" target="_blank" rel="noopener">Pico CSS</a> ✨.</small>
-      </section>
-    </footer>
+  <footer class="container">
+    <a href="#top">Go to top</a>
+    <hr >
+    <section>
+      <small
+        >This website is made with care using
+        <a href="https://nuxt.com" target="_blank" rel="noopener">Nuxt</a> and
+        <a href="https://picocss.com" target="_blank" rel="noopener"
+          >Pico CSS</a
+        >
+        ✨. Learn more about it in the
+        <a
+          href="https://github.com/jackyscript/jackyscript"
+          target="_blank"
+          rel="noopener"
+          >code repository</a
+        >.
+      </small>
+    </section>
+  </footer>
 </template>
