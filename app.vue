ee2ac4c7fe93bd35d1aeca0e0cf390cff3c0b3e5
jackyscript <8930712+jackyscript@users.noreply.github.com>
2025-06-17 01:00:13 +0200

    add spark

--- app.vue
+++ app.vue
@@ -12,9 +12,8 @@
         >This website is made with care using
         <a href="https://nuxt.com" target="_blank" rel="noopener">Nuxt</a> and
         <a href="https://picocss.com" target="_blank" rel="noopener"
-          >Pico CSS</a
-        >
-        ✨. Learn more about it in the
+          >Pico CSS <svgo-spark aria-hidden="true" alt="Spark icon" /></a
+        >. Learn more about it in the
         <a
           href="https://github.com/jackyscript/jackyscript"
           target="_blank"
@@ -25,3 +24,9 @@
     </section>
   </footer>
 </template>
+
+<style scoped>
+svg {
+  width: 1rem;
+}
+</style>
