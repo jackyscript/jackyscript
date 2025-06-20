7bf7e975fa9231bbfa43724d616f956be1b148b1
jackyscript <8930712+jackyscript@users.noreply.github.com>
2025-06-20 19:37:44 +0200

    add footer

--- app.vue
+++ app.vue
@@ -7,13 +7,18 @@
   <footer class="container">
     <a href="#top">Go to top</a>
     <hr >
-    <section>
+    <section class="grid">
       <small
         >This website is made with care using
         <a href="https://nuxt.com" target="_blank" rel="noopener">Nuxt</a> and
         <a href="https://picocss.com" target="_blank" rel="noopener"
           >Pico CSS <svgo-spark aria-hidden="true" alt="Spark icon" /></a
-        >. Learn more about it in the
+        >.
+      </small>
+    </section>
+    <section>
+      <small
+        >Learn more about it in the
         <a
           href="https://github.com/jackyscript/jackyscript"
           target="_blank"
@@ -22,11 +27,28 @@
         >.
       </small>
     </section>
+    <section>
+      <small>
+        © {{ $dayjs().format("YYYY") }} Dezhi Jacky Fu.
+        <a
+          href="https://creativecommons.org/licenses/by-sa/4.0/"
+          target="_blank"
+          rel="noopener"
+          >CC BY-SA 4.0</a
+        >
+      </small>
+    </section>
   </footer>
 </template>
 
 <style scoped>
 svg {
   width: 1rem;
+}
+
+footer.container section {
+  width: 100%;
+  display: flex;
+  justify-content: center;
 }
 </style>
