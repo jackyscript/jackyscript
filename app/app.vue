abce3b254f11e3a1667fab4776d208a8ae9e3cc3
dejafu <8930712+jackyscript@users.noreply.github.com>
2025-08-05 18:36:55 +0200

    Update app.vue

--- app/app.vue
+++ app/app.vue
@@ -1,46 +1,4 @@
 <template>
   <NuxtRouteAnnouncer />
-
   <NuxtPage />
-
-  <footer class="container">
-    <section class="grid">
-      <small
-        >This website is made with care using
-        <a href="https://nuxt.com" target="_blank" rel="noopener">Nuxt</a> and
-        <a href="https://picocss.com" target="_blank" rel="noopener"
-          >Pico CSS <svgo-spark aria-hidden="true" alt="Spark icon" /></a
-        >.
-      </small>
-    </section>
-    <section>
-      <small
-        >Learn more about it in the
-        <a
-          href="https://github.com/jackyscript/jackyscript"
-          target="_blank"
-          rel="noopener"
-          >code repository</a
-        >.
-      </small>
-    </section>
-    <section>
-      <small>
-        © {{ $dayjs().format("YYYY") }} Dezhi Jacky Fu.
-        <a
-          href="https://creativecommons.org/licenses/by-sa/4.0/"
-          target="_blank"
-          rel="noopener"
-          >CC BY-SA 4.0</a
-        >
-      </small>
-    </section>
-  </footer>
 </template>
-
-<style scoped>
-svg {
-  width: 1rem;
-}
-
-</style>
