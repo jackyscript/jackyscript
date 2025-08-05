73538ce1a7e7b37f0a7544b9c4010e02e70cd33a
dejafu <8930712+jackyscript@users.noreply.github.com>
2025-08-05 18:01:27 +0200

    Update TheContact.vue

--- app/components/TheContact.vue
+++ app/components/TheContact.vue
@@ -1,4 +1,4 @@
 <template>
   <h1>Hello</h1>
-  <p>I moved to codeberg, feel free <a href="https://dejafu.codeberg.page/>to go there</a> instead.</p>
+  <p>I moved to codeberg, feel free <a href="https://dejafu.codeberg.page/">to go there</a> instead.</p>
 </template>
